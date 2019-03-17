56cbb34de2dfafa121fb507b55385f279b4e5c47
Gabriel Jo <gabrieljo1302@gmail.com>
2019-03-17 15:26:00 +1300

    added sendgrid emailer

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,35 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "gabrieljo1302@gmail.com",
+    to: address,
+    subject: "Login Secret for Prisma",
+    html: `Hello! Your login secret is ${secret} <br />Compay paste on the app to login`
+  };
+
+  return sendMail(email);
 };
